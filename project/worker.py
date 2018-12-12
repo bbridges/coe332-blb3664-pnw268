b685a4577426b571a5aa681fdd329d755b2aed8e
Bradley Bridges <blbridges96@gmail.com>
2018-12-12 17:34:07 -0600

    Fetch data in worker and add skeleton for making plots

--- project/worker.py
+++ project/worker.py
@@ -1,6 +1,8 @@
 import os
+import io
 
 import redis
+import requests
 
 import jobs
 
@@ -8,6 +10,8 @@ import jobs
 redis_client = redis.StrictRedis(host=os.environ['REDIS_HOST'],
                                  port=os.environ['REDIS_PORT'],
                                  db=os.environ['REDIS_DB'])
+
+API_BASE = f"http://{os.environ['API_HOST']}:{os.environ['API_PORT']}"
 
 
 def start_worker():
@@ -18,14 +22,33 @@ def start_worker():
     """
     while True:
         job_id = jobs.get_new_job(redis_client)
-        job_dict = jobs.get_job(redis_client, job_id)
-
-        jobs.update_status(redis_client, job_id, 'processing')
-
-        _handle_new_job(job_dict)
-
-        jobs.update_status(redis_client, job_id, 'completed')
+        _handle_job_id(job_id)
 
 
-def _handle_new_job(job_dict):
-    print('TODO: actually do something here')
+def _handle_job_id(job_id):
+    jobs.update_status(redis_client, job_id, 'processing')
+
+    job_dict = jobs.get_job(redis_client, job_id)
+    data = _get_data(job_dict)
+
+    plot = _create_plot(data)
+    jobs.update_plot(redis_client, job_id, plot)
+
+    jobs.update_status(redis_client, job_id, 'completed')
+
+
+def _get_data(job_dict):
+    fields = ('start', 'end', 'limit', 'offset')
+    params = {field: job_dict.get(field) for field in fields
+              if job_dict.get(field) is not None}
+    return requests.get(f'{API_BASE}/spots', params=params).json()
+
+
+def _create_plot(data):
+    file = io.BytesIO()
+
+    # TODO: do all the plotting stuff here and save to the BytesIO
+    #       object above.
+    file.write(b'todo')
+
+    return file.getvalue()
